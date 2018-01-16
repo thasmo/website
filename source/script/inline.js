9ecc1478b77b1027a04b8d1c577e62d034b063c8
Thomas Deinhamer <thasmo@gmail.com>
2018-01-16 22:13:31 +0100

    Inline all JS

--- source/script/inline.js
+++
@@ -1,4 +0,0 @@
-// inline.js
-
-// = include ../../node_modules/fg-loadcss/src/loadCSS.js
-// = include ../../node_modules/fg-loadcss/src/cssrelpreload.js
--- source/script/main.js
+++ source/script/main.js
@@ -1,3 +1,5 @@
-// main.js
+// inline.js
 
+// = include ../../node_modules/fg-loadcss/src/loadCSS.js
+// = include ../../node_modules/fg-loadcss/src/cssrelpreload.js
 // = include component/worker.js
