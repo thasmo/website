18617ec4b7070e8944b425e4cf1eec58438e050b
Thomas Deinhamer <thasmo@thasmo.com>
2019-05-18 19:08:46 +0200

    Configure web-fonts to swap after being loaded

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/style/main.scss';
 
 const resources = {
 	manifest: '/application.webmanifest',
-	fonts: 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700%7cRoboto+Slab:300,400',
+	fonts: 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700%7cRoboto+Slab:300,400&display=swap',
 	icon: 'https://gravatar.com/avatar/39263d15a3b0838a938aaaa59dde5c06?s=256',
 };
 
