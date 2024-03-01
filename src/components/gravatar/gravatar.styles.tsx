606a462e2ee13a165692d6ace4c70f506de5f9f8
Thomas Deinhamer <code@thasmo.dev>
2024-03-01 13:29:32 +0100

    Remove low resolution gravatar preview image

--- src/components/gravatar/gravatar.styles.tsx
+++ src/components/gravatar/gravatar.styles.tsx
@@ -4,13 +4,6 @@ export const gravatar = css({
 	display: 'grid',
 });
 
-export const preview = css({
-	position: 'relative',
-	zIndex: 0,
-	gridArea: '1/-1',
-	filter: 'blur(8px)',
-});
-
 export const image = css({
 	position: 'relative',
 	zIndex: 1,
--- src/components/gravatar/gravatar.tsx
+++ src/components/gravatar/gravatar.tsx
@@ -1,5 +1,5 @@
 import {component$, type PropsOf, useSignal, useTask$} from '@builder.io/qwik';
-import {generatePlaceholder, generateSource} from '~/components/gravatar/gravatar.utilities';
+import {generateSource} from '~/components/gravatar/gravatar.utilities';
 
 import * as styles from './gravatar.styles';
 
@@ -9,13 +9,8 @@ export type GravatarProperties = PropsOf<'img'> & {
 };
 
 export default component$<GravatarProperties>(({ email, size, ...props}) => {
-	const preview = useSignal<string>();
 	const low = useSignal<string>();
 	const high = useSignal<string>();
-
-	useTask$(async () => {
-		preview.value = await generatePlaceholder(email, size / 10);
-	});
 
 	useTask$(async () => {
 		low.value = 'https://thasmo.com/.netlify/images?url=' + await generateSource(email, size);
@@ -24,13 +19,6 @@ export default component$<GravatarProperties>(({ email, size, ...props}) => {
 
 	return (
 		<div class={styles.gravatar}>
-			<img class={styles.preview}
-				 src={`data:image/png;base64,${preview.value}`}
-				 width={size}
-				 height={size}
-				 decoding="async"
-				 {...props} />
-
 			<img class={styles.image}
 				 src={low.value}
 				 srcset={`${low.value} 1x, ${high.value} 2x`}
