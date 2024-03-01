cf90207804e6bab539c91baea3fed3e8ef66fc28
Thomas Deinhamer <code@thasmo.dev>
2024-03-01 14:47:20 +0100

    Update headline component

--- src/components/headline/headline.tsx
+++ src/components/headline/headline.tsx
@@ -10,7 +10,7 @@ export default component$<HeadlineProperties>(({ level = 1, tag, ...props}) => {
 	const Headline = tag || `h${level}`;
 	const [styleProps, componentProps] = headline.splitVariantProps({ level, ...props });
 
-	componentProps.class = cx(headline(styleProps));
+	componentProps.class = cx(headline(styleProps), componentProps.class as string);
 
 	return (
 		<Headline {...componentProps}>
